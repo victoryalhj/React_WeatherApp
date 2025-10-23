fec9c82e6ca7db66af222582b8e660d5e32c4555
victoryalhj <victoryalhj@gmail.com>
2025-10-23 21:13:02 +0100

    fix: resolve Vercel deployment error

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const WeatherBox = ({weather}) => {
+  if(!weather) {
+    return <div>Loading......</div>
+  }
   console.log('weather',weather)
   return (
     <div className='weather-box'>
       <div>{weather?.name}</div>
       <h2>{weather?.main?.temp}°C/ {(weather?.main?.temp)*1.8+32} °F</h2>
-      <h1>{weather?.weather[0].description}</h1>
+      <h1>{weather?.weather?.[0]?.description}</h1>
 
     </div>
   )
